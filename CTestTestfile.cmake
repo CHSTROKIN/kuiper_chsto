176e049b77e9897c7759ccac24b5ae8c675d61d8
haotian qi <chstrokin@gmail.com>
2025-05-03 17:46:23 +0100

    Feat: Implement the cuda adding function and cpu adding function, refine the kernel

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/haotian/kuiper_chsto
+# Build directory: /home/haotian/kuiper_chsto
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
+subdirs("demo")
